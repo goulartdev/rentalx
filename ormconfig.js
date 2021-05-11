8872833848c0e582808ee5f514d97ef254756baa
Djonathan Goulart <d.goulart@outlook.com.br>
2021-05-11 08:44:59 -0300

    refactor: added .env to configure application; added script to create test database;

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,12 @@
-const SnakeNamingStrategy = require("typeorm-naming-strategies").SnakeNamingStrategy
+const { SnakeNamingStrategy } = require("typeorm-naming-strategies");
 
 module.exports = {
   "type": "postgres",
-  "host": "db",
-  "port": 5432,
-  "username": "rentalx",
-  "password": "rentalx",
-  "database": "rentalx",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "username": process.env.DB_USER,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
   "namingStrategy": new SnakeNamingStrategy,
   "entities": [
     "./src/modules/**/entities/*.ts",
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,28 +1,3 @@
-import "reflect-metadata";
-import express from "express";
-import "express-async-errors";
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
+import app from "./app";
 
-import createConnection from "@externals/typeorm";
-
-import errorHandler from "./middlewares/error-handler";
-import router from "./routes";
-import "@shared/container";
-import "@shared/providers";
-
-createConnection();
-
-const swaggerDoc = YAML.load(`${__dirname}/../swagger.yaml`);
-
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
-
-app.use(router);
-app.use(errorHandler);
-
-// todo: colocar port em uma variável de ambiente
-app.listen(3000, () => console.log("Server is running on port 3000..."));
+app.listen(process.env.APP_PORT, () => console.log("Server is running on port 3000..."));
