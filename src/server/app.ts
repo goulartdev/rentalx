457c5470877f05aeb249ea9c3a2a194370a7d489
Djonathan Goulart <d.goulart@outlook.com.br>
2021-07-17 21:00:16 -0300

    added rate limiter and sentry handler

--- src/server/app.ts
+++ src/server/app.ts
@@ -7,8 +7,11 @@ import YAML from "yamljs";
 
 import upload from "@config/upload";
 import createConnection from "@externals/typeorm";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import errorHandler from "./middlewares/error-handler";
+import rateLimiter from "./middlewares/rate-limiter";
 import router from "./routes";
 import "@shared/container";
 import "@shared/providers";
@@ -19,6 +22,20 @@ const swaggerDoc = YAML.load(`${__dirname}/../swagger.yaml`);
 
 const app = express();
 
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
@@ -27,6 +44,9 @@ app.use("/img", express.static(`${upload.tmpFolder}`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
+
 app.use(errorHandler);
 
 export default app;
---
+++ src/server/middlewares/rate-limiter.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import redis from "redis";
+
+import AppError from "@shared/errors/app-error";
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "middleware",
+  points: 10,
+  duration: 5,
+});
+
+async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError("Too many requests", 429);
+  }
+}
+
+export default rateLimiter;
